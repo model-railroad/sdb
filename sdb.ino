42664b9590c574163b0b83ed05e80bed07400d19
ralf <ralfoide@gmail.com>
2023-02-18 21:29:41 -0800

    ESP32: Blinky skeleton.

--- sdb.ino
+++ sdb.ino
@@ -3,20 +3,24 @@
 
 #include <HardwareSerial.h>
 
+//import something;
+
 #define DEBUG_PRINTF(x)   { Serial.printf x ; }
 
 void setup() {
     Serial.begin(115200);
     Serial.setDebugOutput(true);
     pinMode(BUILTIN_LED, OUTPUT);
-    DEBUG_PRINTF( ("Grade Crossing INO running on Core %d\n", xPortGetCoreID()) );
-
+    pinMode(19, OUTPUT);
+    DEBUG_PRINTF( ("SDB running on Core %d, compiled using C++ %d\n", xPortGetCoreID(), __cplusplus) );
 }
 
 void loop() {
-    DEBUG_PRINTF( ("loop led pin %d\n", BUILTIN_LED) );
-    digitalWrite(BUILTIN_LED, LOW);
-    delay(250 /*ms*/);
+    DEBUG_PRINTF( ("loop led pin 19+%d\n", BUILTIN_LED) );
     digitalWrite(BUILTIN_LED, HIGH);
+    digitalWrite(19, HIGH);
+    delay(250 /*ms*/);
+    digitalWrite(BUILTIN_LED, LOW);
+    digitalWrite(19, LOW);
     delay(1000 /*ms*/);
 }
