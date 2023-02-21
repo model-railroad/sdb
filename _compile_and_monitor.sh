606a4603be3bf88dccaec12b73cec6b496ae90ef
ralf <ralfoide@gmail.com>
2023-02-20 16:05:23 -0800

    ESP32: Use arduino-cli compile sketch profile.

--- _compile_and_monitor.sh
+++ _compile_and_monitor.sh
@@ -1,3 +1,3 @@
 date
 pwd
-./_arduino_cli.sh compile -u && ./_arduino_cli.sh monitor -p COM5 -c baudrate=115200
+./_arduino_cli.sh compile --upload --profile wifikit32 && ./_arduino_cli.sh monitor -p COM5 -c baudrate=115200
