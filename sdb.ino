b6f5fe1958f61fd7a78c3a9ae775b50986a09d96
ralf <ralfoide@gmail.com>
2023-03-05 17:22:27 -0800

    SDB: Fix sdbPanic() to halt core tasks.

--- sdb.ino
+++ sdb.ino
@@ -5,19 +5,36 @@
 #include "src/mod_blinky.h"
 #include "src/mod_display.h"
 #include "src/mod_tof.h"
+#include "src/mod_wifi.h"
 
 SdbModManager _gSdbModManager;
 
 void sdbPanic(char* msg) {
     if (msg != NULL) {
         ERROR_PRINTF( ("[SDB] PANIC! Cause: %s\n", msg ) );
+    } else {
+        ERROR_PRINTF( ("[SDB] PANIC! Please reset.\n" ) );
     }
+
+    // Suspend task switching on *this* core.
+    //
+    // Note: we cannot suspend tasks on the other core. There's no API for it.
+    // We could create a task with max priority on the other core and schedule it.
+    //
+    // Note: don't call rtDelay(), delay(), sleep(), usleep(), or Serial.printf()
+    // after vTaskSuspendAll().
+    vTaskSuspendAll();
+
     pinMode(LED_BUILTIN, OUTPUT);
+    long delayMS = 150;
+    bool state = false;
     while (true) {
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(500 /*ms*/);
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(500 /*ms*/);
+        long endMS = millis() + delayMS;
+        digitalWrite(LED_BUILTIN, state ? HIGH : LOW);
+        while (millis() < endMS) {
+            NOP();
+        }
+        state = !state;
     }
 }
 
@@ -33,9 +50,11 @@ void setup() {
     auto blinky = new SdbModBlinky(_gSdbModManager);
     auto display = new SdbModDisplay(_gSdbModManager);
     auto tof = new SdbModTof(_gSdbModManager);
+    auto wifi = new SdbModWifi(_gSdbModManager);
     _gSdbModManager.registerMod(blinky);
     _gSdbModManager.registerMod(display);
     _gSdbModManager.registerMod(tof);
+    _gSdbModManager.registerMod(wifi);
     _gSdbModManager.onStart();
 }
 
