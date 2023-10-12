057857afe2a4304f97d42217e6a82ecdb7769301
ralf <ralfoide@gmail.com>
2023-10-11 20:47:58 -0700

    SDB: Monitor backtrace & decode it.

---
+++ _monitor.sh
@@ -0,0 +1,14 @@
+ADDR2LINE=$(grep "g++" build/compile_commands.json | sort -u | head -n 1 | tr -d \", | sed 's/g++/addr2line/')
+PORT=COM5
+echo "Monitor on $PORT"
+./_arduino_cli.sh monitor -p $PORT -c baudrate=115200 2>&1 | while read -r LINE ; do
+  echo "$LINE"
+  if [[ "$LINE" =~ ^Backtrace: ]]; then
+    echo
+    $ADDR2LINE -pfiaC -e build/sdb.ino.elf ${LINE:10}
+    echo
+    echo "Paused 5 seconds"
+    sleep 5s
+    echo
+  fi
+done
