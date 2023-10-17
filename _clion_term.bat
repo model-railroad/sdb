a833e34f25851a302c229d746acc74d5c766655e
ralf <ralfoide@gmail.com>
2023-10-16 22:31:04 -0700

    SDB: Update scripts.

--- _clion_term.bat
+++ _clion_term.bat
@@ -1,3 +1,15 @@
-@echo off
-set CHERE_INVOKING=1
-C:\msys64\usr\bin\bash.exe --login
+@ECHO OFF
+SET CHERE_INVOKING=1
+IF EXIST C:\msys64\usr\bin\bash.exe (
+    echo Starting msys2 bash
+    C:\msys64\usr\bin\bash.exe --login
+    GOTO :eof
+)
+IF EXIST C:\cygwin64\bin\bash.exe (
+     echo Starting cygwin bash
+     C:\cygwin64\bin\bash.exe --login
+     GOTO :eof
+) ELSE (
+    echo Starting git windows bash
+    "C:\Program Files\Git\bin\bash.exe"
+)
