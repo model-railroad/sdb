55374c30111bc2e229f41cb2cf7e4fc6929425a8
ralf <ralfoide@gmail.com>
2024-10-04 23:52:35 -0700

    SDB: Add sketch.local for profile+COM port selection.

--- _compile.sh
+++ _compile.sh
@@ -2,14 +2,26 @@ date
 pwd
 FLAGS="-Os"
 
-# Select Profile from sketch.yaml
-# PROF="wifikit32"
-PROF="esp32cam"
+# Select Profile...
+# Override the profile+port by writing its name in a file "sketch.local" (which is not checked in git)
+if [[ -f sketch.local ]]; then
+  PROF=$(cat sketch.local | awk '{print $1}' | tr -d -c "[a-z0-9]")
+  PORT=$(cat sketch.local | awk '{print $2}')
+fi
+if [[ -z "$PROF" ]]; then
+  PROF="wifikit32"
+fi
+if [[ -z "$PORT" ]]; then
+  PORT=$(grep default_port sketch.yaml | awk '{print $2}')
+fi
+
 if [[ "$1" == "-g" ]]; then
   FLAGS="-g -O0"
   PROF="${PROF}_debug"
   shift
 fi
+
+echo "Sketch.yaml profile $PROF on port $PORT"
 
 if [[ -d src/cmake-build-debug ]]; then
   for F in $(find src/cmake-build-debug/ -name "*\.c" -or -name "*\.cpp"); do
@@ -20,7 +32,9 @@ fi
 ./_gen_html_gz.sh src/html/_mod_wifi_ap_index.html
 ./_gen_html_gz.sh src/html/_mod_wifi_sta_index.html
 ./_gen_html_gz.sh src/html/_mod_wifi_style.css
-./_arduino_cli.sh compile $@ --build-path ./build --profile $PROF \
+./_arduino_cli.sh compile $@ --build-path ./build \
+    --profile $PROF \
+    -p $PORT \
     --build-property "build.extra_flags=$FLAGS" \
     --build-property "compiler.cpp.extra_flags=-DESP32" \
     --build-property "compiler.cpp.extra_flags=-DESP32_PROFILE_$PROF"
--- _monitor.sh
+++ _monitor.sh
@@ -1,8 +1,11 @@
 ADDR2LINE=$(grep "g++" build/compile_commands.json | sort -u | head -n 1 | tr -d \", | sed 's/g++/addr2line/')
 if [[ "$1" =~ COM* ]]; then
-	PORT="$1"
+  PORT="$1"
 else
-	PORT=$(grep default_port sketch.yaml | awk '{print $2}')
+  PORT=$(cat sketch.local | awk '{print $2}')
+  if [[ -z "$PORT" ]]; then
+    PORT=$(grep default_port sketch.yaml | awk '{print $2}')
+  fi
 fi
 echo "Monitor on $PORT"
 ./_arduino_cli.sh monitor -p $PORT -c baudrate=115200 2>&1 | while read -r LINE ; do
