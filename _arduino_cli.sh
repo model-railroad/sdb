dc7ccd145cbd6518ecea366cc9f18555daff3669
ralf <ralfoide@gmail.com>
2024-11-12 13:28:11 -0800

    SDB: Use local arduino-cli if installed.

--- _arduino_cli.sh
+++ _arduino_cli.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-C=( $(cygpath "$USERPROFILE")/.vscode/extensions/vsciot-vscode.vscode-arduino-*-win32-*/assets/platform/win32-*/arduino-cli/arduino-cli.exe )
-C="${C[0]}"
+
+# Search for Arduino CLI in the exec PATH
+C=$(which arduino-cli.exe)
+
+if [[ ! -x "$C" ]]; then
+	C=$(which arduino-cli)
+fi
+
+if [[ ! -x "$C" ]]; then
+	# VS Code Arduino Extension by MS:     vsciot-vscode.vscode-arduino
+	# VS Code Arduino Community Extension: vscode-arduino.vscode-arduino-community
+	C=( $(cygpath "$USERPROFILE")/.vscode/extensions/*.vscode-arduino-*-win32-*/assets/platform/win32-*/arduino-cli/arduino-cli* )
+	C="${C[0]}"
+fi
 if [[ ! -x "$C" ]]; then
     echo "ERROR: arduino-cli not found in $USERPROFILE/.vscode/extensions"
     exit 1
 fi
+"$C" version
 "$C" "$@"
 
